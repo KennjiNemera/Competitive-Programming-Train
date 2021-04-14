8c49e4c1e382f655b020ac44e3f8d13a64ee6c2d
Kenna <65411099+KennjiNemera@users.noreply.github.com>
2021-04-15 00:09:13 +0200

    Daily Push

---
+++ Codeforces/Div2_B/Coins_54B.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.util.*;
+
+public class Coins_54B {
+  public static void main(String[] args) throws IOException{
+    FastReader fr = new FastReader();
+    PrintWriter pr = new PrintWriter(new OutputStreamWriter(System.out));
+    int n = fr.nextInt();
+
+    if (n == 1) {
+      pr.println(1);
+    } else {
+      int i = 2;
+      int start = 1;
+      int end = n;
+      ArrayList<Integer> arr = new ArrayList<>();
+
+      arr.add(1);
+
+      while (i <= n && start < n) {
+        if (i % start == 0 && end % i == 0) {
+          arr.add(i);
+          start = i;
+        }
+        i++;
+      }
+
+      for (int j = arr.size()-1; j >= 0; j--) {
+        pr.print(arr.get(j) + " ");
+      }
+    }
+
+    pr.close();
+  }
+
+  static class Pair {
+      int x, y;
+      public Pair(int x, int y) {
+          this.x = x;
+          this.y = y;
+      }
+  }
+
+  static int toInt(String s) {
+    return Integer.parseInt(s);
+  }
+
+  static class FastReader 
+  { 
+      BufferedReader br; 
+      StringTokenizer st; 
+
+      public FastReader() throws FileNotFoundException 
+      { 
+          br = new BufferedReader(new InputStreamReader(System.in)); 
+      } 
+
+      String next() 
+      { 
+          while (st == null || !st.hasMoreElements()) 
+          { 
+              try
+              { 
+                  st = new StringTokenizer(br.readLine()); 
+              } 
+              catch (IOException  e) 
+              { 
+                  e.printStackTrace(); 
+              } 
+          } 
+          return st.nextToken(); 
+      } 
+
+      int nextInt() 
+      { 
+          return Integer.parseInt(next()); 
+      } 
+
+      long nextLong() 
+      { 
+          return Long.parseLong(next()); 
+      } 
+
+      double nextDouble() 
+      { 
+          return Double.parseDouble(next()); 
+      } 
+
+      String nextLine() 
+      { 
+          String str = ""; 
+          try
+          { 
+              str = br.readLine(); 
+          } 
+          catch (IOException e) 
+          { 
+              e.printStackTrace(); 
+          } 
+          return str; 
+      } 
+  }
+}
