252b84e6a6d1c28bdd68f0554294bcb3eddff0ce
Kenna <kennanemera.web@gmail.com>
2021-06-09 22:48:13 +0200

    Push

---
+++ Codeforces/Misc/FlippingGame.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.*;
+
+public class FlippingGame {
+  public static void main(String[] args) throws IOException{
+    FastReader fr = new FastReader();
+    PrintWriter pr = new PrintWriter(new OutputStreamWriter(System.out));
+    int n = fr.nextInt();
+
+    int[] pre = new int[n+1];
+
+    int total = 0;
+
+    for (int i = 1; i < pre.length; i++) {
+        int a = fr.nextInt();
+
+        if (a == 1) {
+            total++;
+        }
+
+        pre[i] = total;
+    }
+
+    // brute force for the maximum range
+    int max = Integer.MIN_VALUE;
+
+    for (int i = 1; i < pre.length; i++) {
+        for (int j = i; j < pre.length; j++) {
+            int val = pre[i-1] + ((j-i+1)-(pre[j]-pre[i-1])) + (pre[n] - pre[j]);
+            max = Math.max(max, val);        
+        }
+    }
+
+    pr.println(max);
+    pr.close();
+  }
+
+  static class Pair {
+      int x, y;
+      public Pair(int x, int y) {
+          this.x = x;
+          this.y = y;
+      }
+  }
+
+  static int toInt(String s) {
+    return Integer.parseInt(s);
+  }
+
+  static class FastReader 
+  { 
+      BufferedReader br; 
+      StringTokenizer st; 
+
+      public FastReader() throws FileNotFoundException 
+      { 
+          br = new BufferedReader(new InputStreamReader(System.in)); 
+      } 
+
+      String next() 
+      { 
+          while (st == null || !st.hasMoreElements()) 
+          { 
+              try
+              { 
+                  st = new StringTokenizer(br.readLine()); 
+              } 
+              catch (IOException  e) 
+              { 
+                  e.printStackTrace(); 
+              } 
+          } 
+          return st.nextToken(); 
+      } 
+
+      int nextInt() 
+      { 
+          return Integer.parseInt(next()); 
+      } 
+
+      long nextLong() 
+      { 
+          return Long.parseLong(next()); 
+      } 
+
+      double nextDouble() 
+      { 
+          return Double.parseDouble(next()); 
+      } 
+
+      String nextLine() 
+      { 
+          String str = ""; 
+          try
+          { 
+              str = br.readLine(); 
+          } 
+          catch (IOException e) 
+          { 
+              e.printStackTrace(); 
+          } 
+          return str; 
+      } 
+  }
+}
