3e3d2c65f7ef21fa80ae7711c3ba1dad48310468
Kenna <kennanemera.web@gmail.com>
2023-11-18 00:03:48 +0200

    update

--- Codeforces/ProblemSet/Data Structures/LabourHeracles.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-// Unsolved due to lack of understanding: Involves GREEDY ALGO
-public class LabourHeracles {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-	}
-}
---
+++ atcoder/b252/test.cpp
@@ -0,0 +1,45 @@
+// state the problem source, problem name / contest, date solved 
+// [...state the problem tags/topics...]
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define vi vector<int>
+#define pi pair<int, int>
+#define f first
+#define s second
+#define pb push_back
+#define vll vector<long long>
+#define FOR(i, x, y) for(int i = x; i < y; i++)
+#define set(vect, val) fill(begin(vect), end(vect), val)
+
+typedef long long int64;
+
+void solve()
+{
+    // understand the problem
+    // work thorugh the test cases
+    // see if you can find some problem group or fixed algo
+    // psuedo
+    // implement
+
+    cout << "hello world" << endl;
+}
+
+int main()
+{
+    auto start = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    for(int i = 0; i < t; i++){
+        // cout << "Case #" << i + 1 << ": ";
+        solve();
+    }
+    auto stop = chrono::high_resolution_clock::now(); 
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    cout <<"Time taken : "<<duration.count() <<" milliseconds\n";
+}
+
